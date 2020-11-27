7d15cc72621e017dde76716419bfb96e36353c61
Elliot Lewis <elliotleelewis@gmail.com>
2020-11-27 13:14:04 -0500

    feat: added page routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ForecastComponent } from './forecast/forecast.component';
 import { HomeComponent } from './home/home.component';
 
+const ROUTES: Routes = [
+  { path: 'forecast/:zipcode', component: ForecastComponent },
+  { path: '', pathMatch: 'full', component: HomeComponent },
+];
+
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(ROUTES, { useHash: true }),
+  ],
   declarations: [AppComponent, HomeComponent, ForecastComponent],
   bootstrap: [AppComponent],
 })
