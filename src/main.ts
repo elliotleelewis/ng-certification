83b85e5d1aa5ea1d669c70f873492b6a0d5dd920
Elliot Lewis <elliotleelewis@gmail.com>
2020-11-28 13:53:01 -0500

    feat: upgraded to Angular 11

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,7 @@
-import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import './polyfills';
-
-declare const window: Window & { ngRef: any };
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
-  .then((ref) => {
-    // Ensure Angular destroys itself on hot reloads.
-    if (window.ngRef) {
-      window.ngRef.destroy();
-    }
-    window.ngRef = ref;
-
-    // Otherwise, log the boot error
-  })
   .catch((err) => console.error(err));
